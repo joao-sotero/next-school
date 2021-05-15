d409da03a80cd3d27e6465d05bd40ecd42060d00
joao-sotero <vitor99jjunior@gmail.com>
2021-05-14 22:29:13 -0300

    retirei o config do ignore

---
+++ config/config.js
@@ -0,0 +1,15 @@
+// module.exports = {
+//   "username": "nextschool@nextschoolbd",
+//   "password": "Next1234",
+//   "database": "nextschool",
+//   "host": "nextschoolbd.mysql.database.azure.com",
+//   "dialect": "mysql"
+// }
+
+module.exports = {
+  "username": "root",
+  "password": "javasalva",
+  "database": "next-school",
+  "host": "127.0.0.1",
+  "dialect": "mysql"
+}
